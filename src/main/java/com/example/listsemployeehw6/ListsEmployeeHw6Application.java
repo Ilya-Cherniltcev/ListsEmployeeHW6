afc432d09b68cb6584ed6f56224b91fab6968ab6
Ilya Cherniltsev <cherniltcev@mail.ru>
2022-05-04 20:47:32 +0500

    first commit

---
+++ src/main/java/com/example/listsemployeehw6/ListsEmployeeHw6Application.java
@@ -0,0 +1,13 @@
+package com.example.listsemployeehw6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListsEmployeeHw6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListsEmployeeHw6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/listsemployeehw6/ListsEmployeeHw6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listsemployeehw6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsEmployeeHw6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
